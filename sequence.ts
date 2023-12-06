63cbbce215ac1563b0edb6f15d7526ca1c3a4260
HoomanHQ <126047668+HoomanHQ@users.noreply.github.com>
2023-12-06 19:29:48 +0530

    Add files via upload

---
+++ sequence.ts
@@ -0,0 +1,36 @@
+export const docsSequence = [
+  {
+    label: "User",
+    subItems: [
+      { label: "Getting Started" },
+      { label: "Guides" },
+      {
+        label: "Providers",
+        subItems: [
+          { label: "Enabling Persistent Storage" },
+          {
+            label: "Build",
+            subItems: [
+              {
+                label: "Test Build",
+                subItems: [
+                  { label: "Overview Copy" },
+                  { label: "Build a Cloud Provider" },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    label: "Providers",
+    subItems: [
+      { label: "Deployments" },
+      { label: "Concepts" },
+      { label: "Architecture" },
+    ],
+  },
+];
